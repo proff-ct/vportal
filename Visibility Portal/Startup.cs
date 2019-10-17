6682066f3cb564fb76fb9af27e31ac38d85f88ae
Matthew Adote <madote@coretec.co.ke>
2019-10-17 16:24:50 +0300

    17Oct2019_1622 Add VS solution to the project with ASP Identity

---
+++ Visibility Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Visibility_Portal.Startup))]
+namespace Visibility_Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
