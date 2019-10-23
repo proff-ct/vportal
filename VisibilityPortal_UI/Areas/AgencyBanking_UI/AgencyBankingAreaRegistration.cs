9612cad91726a10989c49b7c82a0ea6f5f6f5339
Matthew Adote <madote@coretec.co.ke>
2019-10-23 13:31:10 +0300

    23Oct2019_1327 Add modular structure for: AgencyBanking, MSacco
    Note(s):
        * Tested that this works ok

---
+++ VisibilityPortal_UI/Areas/AgencyBanking_UI/AgencyBankingAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Visibility_Portal
+{
+    public class AgencyBankingAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AgencyBanking_UI";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AgencyBanking_default",
+                "AgencyBanking/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new string[] { "AgencyBanking_BLL.Controllers" }
+            );
+        }
+    }
+}
