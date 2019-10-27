41ef9776d92825d9953aad3747d0eaa9704d473b
Matthew Adote <madote@coretec.co.ke>
2019-10-27 20:17:32 +0300

    27Oct2019_2011 Add project Utilities: Email Domain Validator + 1 more
    Summary:
        * This is to contain any "utilities" / shared resources used across
        the various modules / prpjects making up the Visibility Portal
        * Added:
            1. Coretec email domain validator
            2. Encryption salt string constant

---
+++ Utilities/ValidateCoreTecEmailDomain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Validators.ValidationAttributes
+{
+    public class ValidateCoreTecEmailDomain : ValidationAttribute
+  {
+    private string _domain
+    {
+      get
+      {
+        return "@coretec.co.ke";
+      }
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+      if (value != null)
+      {
+        string email = value.ToString();
+        if (email.Substring(email.IndexOf('@')) == _domain)
+        {
+          return ValidationResult.Success;
+        }
+        else
+        {
+          return new ValidationResult("Please enter a valid CoreTec email address");
+        }
+      }
+      else
+      {
+        return new ValidationResult("" + validationContext.DisplayName + " is required");
+      }
+    }
+
+  }
+}
