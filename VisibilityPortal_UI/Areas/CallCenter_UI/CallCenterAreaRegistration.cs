34a7de29454079122cb3f2438391053c9d3508ba
Matthew Adote <madote@coretec.co.ke>
2019-10-28 20:48:17 +0300

    28Oct2019_2042 WIP: Setup Call Center UI
    Summary:
        * Installed Datatables plugin - wanted to recreate the DCS portal
        flow
        * Upgraded jquery to 3.3.1 - for consistency across the UI modules
        * Add Sacco index page - to set the active sacco
        Have since reconsidered this approach
        * Rename _Layout to _LayoutCallCenter - to set this up as the base
        template for the CallCenter_UI module but at the same time use it
        as a child template of the VisibilityPortal_UI _Layout file
    
    Note(s):
        * Encountered issues regarding package references - decision made to
        harmonise VS versions to 2017
        * Tested that all works as expected so far

--- VisibilityPortal_UI/Areas/CallCenter_UI/CallCenterAreaRegistration.cs
+++ VisibilityPortal_UI/Areas/CallCenter_UI/CallCenterAreaRegistration.cs
@@ -2,24 +2,20 @@
 
 namespace Visibility_Portal
 {
-    public class CallCenterAreaRegistration : AreaRegistration 
-    {
-        public override string AreaName 
-        {
-            get 
-            {
-                return "CallCenter_UI";
-            }
-        }
+  public class CallCenterAreaRegistration : AreaRegistration
+  {
+    public override string AreaName => "CallCenter_UI";
 
-        public override void RegisterArea(AreaRegistrationContext context) 
-        {
-            context.MapRoute(
-                "CallCenter_default",
-                "CallCenter/{controller}/{action}/{id}",
-                new { controller = "Homes", action = "Index", id = UrlParameter.Optional },
-                new string[] { "CallCenter_BLL.Controllers" }
-            );
-        }
+    public override void RegisterArea(AreaRegistrationContext context)
+    {
+      context.MapRoute(
+          "CallCenter_default",
+          "CallCenter/{controller}/{action}/{id}",
+          new { controller = "Homes", action = "Index", id = UrlParameter.Optional },
+          new string[] { "CallCenter_BLL.Controllers" }
+      );
     }
+
+    public static string AreaPathFromRoot => "~/Areas/CallCenter_UI";
+  }
 }
