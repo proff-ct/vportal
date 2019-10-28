af1ed714d3235ac2b8520a62a08738dc5e4c09eb
Matthew Adote <madote@coretec.co.ke>
2019-10-28 20:38:17 +0300

    28Oct2019_2037 Setup Call Center DAL, BLL

---
+++ CallCenter_DAL/DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallCenter_DAL
+{
+  public class DBConnection
+  {
+    public DBConnection(string env = "Prod")
+    {
+      switch (env)
+      {
+        case "Prod":
+          ConnectionString = @ConfigurationManager.ConnectionStrings["saccoDB_prod"].ConnectionString;
+          break;
+
+        case "Test":
+          // no test db for the moment
+          ConnectionString = @ConfigurationManager.ConnectionStrings["saccoDB_prod"].ConnectionString;
+          break;
+      }
+
+    }
+
+    public string ConnectionString { get; }
+  }
+}
