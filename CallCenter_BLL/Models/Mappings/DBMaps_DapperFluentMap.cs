4f12cb8a7cfa0bc7cb8b8dfcaeefee6a3fdb5454
Matthew Adote <madote@coretec.co.ke>
2019-10-30 18:33:21 +0300

    30Oct2019_1829 WIP:CallCenter: Add UI to Load Saccos, Set active sacco
    Summary:
        * Add route to fetch the list of saccos
        * Set navigation links on sidebar
    
    Note(s):
        * Tested that everything works ok so far

--- CallCenter_BLL/Models/Mappings/DBMaps_DapperFluentMap.cs
+++ CallCenter_BLL/Models/Mappings/DBMaps_DapperFluentMap.cs
@@ -12,7 +12,7 @@ namespace CallCenter_BLL.Models.Mappings
   {
     internal SaccoMap()
     {
-      Map(s => s.corporateNo).ToColumn("[Corporate No]");
+      Map(s => s.corporateNo).ToColumn("Corporate No");
       Map(s => s.corporateNo_2).ToColumn("Corporate No 2");
       Map(s => s.saccoName_1).ToColumn("Sacco Name 1");
       Map(s => s.mpesaFloat).ToColumn("Mpesa Float");
