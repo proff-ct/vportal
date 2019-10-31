708245a751b205788c4d686797334ae410a4cdb7
Matthew Adote <madote@coretec.co.ke>
2019-10-31 13:01:21 +0300

    31Oct2019_1229 Add config to publish areas too on VisibilityPortal_UI
    Summary:
        * This is on deployment, the areas cannot be found.
        Additionally, manually navigating to /CallCenter resulted in a 404
    
    Note(s):
        * Let's see if this is the final cure

---
+++ webApp/Areas/AgencyBanking_UI/Model/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿namespace AgencyBanking_UI.Model
+{
+    /// <summary>
+    /// A menu item used to hold data.
+    /// </summary>
+   public  class MenuItem
+    {
+        public string Title { get; }
+        public string Imagelink { get; }
+        public string Actioname { get; }
+
+        public MenuItem(string title, string imagelink, string actioname)
+        {
+            Title = title;
+            Imagelink = imagelink;
+            Actioname = actioname;
+        }
+
+    }
+}
---
+++ webApp/Areas/CallCenter_UI/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CallCenter_BLL.Utilities.AutoMapper;
+
+namespace CallCenter_UI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //Mappings.RegisterMappings();
+    }
+    }
+}
