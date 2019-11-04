55bcaede26e73b1853a56ead4b2a6d6b7362f54c
Neptune <eugeneogongo@live.com>
2019-11-04 14:49:12 +0300

    Transaction Type POJO added

---
+++ AgencyBanking_DAL/TransactionModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgencyBanking_DAL
+{
+   public class TransactionModel
+    {
+       public string Amount{get;set;}
+       public string Posted{get;set;}
+       public string DatePosted{get;set;}
+       public string TimePosted{get;set;}
+      
+       public string Description{get;set;}
+       public string AgentCode{get;set;}
+       public string Location{get;set;}
+
+       public string AgentName{get;set;}
+       public string DepositerTelephoneNo{get;set;}
+       public string  DeviceID{get;set;}
+    }
+}
