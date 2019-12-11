aef1186e990bd6f37761fd6ef4dda5383c29bad3
Neptune <eugeneogongo@live.com>
2019-12-11 09:13:36 +0300

    Feature: Added the ability of the sacco to log problems facing their *Machines

---
+++ AgencyBanking_BLL/Controllers/RepairController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using AgencyBanking_DAL;
+using Dapper;
+
+namespace AgencyBanking_BLL.Controllers
+{
+    [Authorize]
+  public  class RepairController: Controller
+    {
+        /// <summary>
+        /// TODO: Use corporate number in future for the saccoid of the currently logged in user
+        /// </summary>
+        /// <returns></returns>
+        //GET: Repair/
+        public ActionResult Index()
+        {
+            var model = new DeviceRepairModel();
+            model.Saccoid = "To be Set";
+            return View(model);
+        }
+
+        //Post: Repair/index
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Index(DeviceRepairModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (CheckImeiExists(model.Imei))
+                    {
+                        new RepairBLL().NewRepair(model);
+                        return Redirect("/");
+                    }
+                    ModelState.AddModelError("Imei", "The Imei was not found");
+                    return View(model);
+
+                }
+            }
+            catch (Exception e)
+            {
+                return Content(e.StackTrace);
+            }
+          
+            return View(model);
+        }
+
+        private bool CheckImeiExists(string imei)
+        {
+            DynamicParameters par =  new DynamicParameters();
+            par.Add("imei",imei);
+            DeviceInfo device = DapperOrm.QueryGetSingle<DeviceInfo>("select * from deviceinfo where imei = @imei", par);
+            return device != null && !string.IsNullOrEmpty(device.IMEI);
+        }
+    }
+}
