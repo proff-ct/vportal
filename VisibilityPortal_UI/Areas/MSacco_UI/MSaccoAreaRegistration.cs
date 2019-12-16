b544e58e37a648949debbb55c4412b175841ffa2
Matthew Adote <madote@coretec.co.ke>
2019-12-16 12:14:32 +0300

    16Dec2019_1210 MSacco: WIP: Add Menu links placeholders to UI
    Summary:
        * This adds the initial view for MSacco UI
    
    Note(s):
        * Tested that the Menus get displayed as expected

--- VisibilityPortal_UI/Areas/MSacco_UI/MSaccoAreaRegistration.cs
+++ VisibilityPortal_UI/Areas/MSacco_UI/MSaccoAreaRegistration.cs
@@ -2,24 +2,20 @@
 
 namespace Visibility_Portal
 {
-    public class MSaccoAreaRegistration : AreaRegistration 
-    {
-        public override string AreaName 
-        {
-            get 
-            {
-                return "MSacco_UI";
-            }
-        }
+  public class MSaccoAreaRegistration : AreaRegistration
+  {
+    public override string AreaName => "MSacco_UI";
 
-        public override void RegisterArea(AreaRegistrationContext context) 
-        {
-            context.MapRoute(
-                "MSacco_default",
-                "MSacco/{controller}/{action}/{id}",
-                new { controller="Home", action = "Index", id = UrlParameter.Optional },
-                new string[] { "MSacco_BLL.Controllers" }
-            );
-        }
+    public override void RegisterArea(AreaRegistrationContext context)
+    {
+      context.MapRoute(
+          "MSacco_default",
+          "MSacco/{controller}/{action}/{id}",
+          new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+          new string[] { "MSacco_BLL.Controllers" }
+      );
     }
+
+    public static string AreaPathFromRoot => "~/Areas/MSacco_UI";
+  }
 }
