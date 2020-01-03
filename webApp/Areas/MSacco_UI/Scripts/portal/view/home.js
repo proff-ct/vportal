9584db9a6423f0bebc62a9884acc48e95624f71f
Matthew Adote <madote@coretec.co.ke>
2020-01-03 14:56:30 +0300

    03Jan2020_1453 Publish MSacco_UI
    Summary:
        * Features:
            + Display data widgets on dashboard for:
                - Loans
                - Withdrawals
                - Utility Payments
                - Airtime Topups
            Display total sum of transaction as at the time the page is
            loaded.
            NB: This feature is WIP
    
    Note(s):
        * Tested that all works as expected

---
+++ webApp/Areas/MSacco_UI/Scripts/portal/view/home.js
@@ -0,0 +1,7 @@
+﻿function DisplayDateNow(dateFormat = null) {
+  return moment().format((dateFormat == null) ? "DD-MMM-YYYY HH:mm:ss" : dateFormat )
+}
+
+function DisplayDefaultQuantity(defaultQty = null) {
+  return defaultQty || "---"
+}
