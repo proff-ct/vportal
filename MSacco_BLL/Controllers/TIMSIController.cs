1db6c6b585da8bea9864bd9bc742ba39100d222f
jandev-tinker <64497780+jandev-tinker@users.noreply.github.com>
2020-09-15 17:44:52 +0300

    15Sep2020_1744 MSACCO: Add IMSI Reset functionality

---
+++ MSacco_BLL/Controllers/TIMSIController.cs
@@ -0,0 +1,70 @@
+﻿using MSacco_Dataspecs.Feature.MsaccoTIMSINumberChecker.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Utilities.PortalApplicationParams;
+using VisibilityPortal_BLL.CustomFilters;
+using VisibilityPortal_Dataspecs.Models;
+
+namespace MSacco_BLL.Controllers
+{
+  [RequireSACCOAdmin]
+  public class TIMSIController: Controller
+  {
+    private IBL_MSACCO_TIMSI_NumberChecker _msaccoTIMSIBLL = new MSaccoTIMSIBLL();
+    // GET: MsaccoPlusNumberCheckers
+    public ActionResult Index()
+    {
+      ViewBag.ActiveUserParams = (ActiveUserParams)Session["ActiveUserParams"];
+      return View();
+    }
+
+    #region Others
+    [HttpGet]
+    [Authorize]
+    public ActionResult GetBlockedTIMSIRecord(string clientCorporateNo, string memberTelephoneNo)
+    {
+      if (string.IsNullOrEmpty(clientCorporateNo) || string.IsNullOrEmpty(memberTelephoneNo))
+      {
+        return null;
+      }
+
+      // the flow:
+      // 1. get the member's record from db
+      // 2. parse and pass the record to client
+      dynamic[] registeredMemberDeviceRecord = {
+        _msaccoTIMSIBLL.GetTIMSIMemberRecordForClient(clientCorporateNo, memberTelephoneNo.Replace("-",""))
+      };
+
+      return Json(registeredMemberDeviceRecord, JsonRequestBehavior.AllowGet);
+    }
+    [HttpPost]
+    [Authorize]
+    public ActionResult ResetMSACCOTIMSIRecord(string clientCorporateNo, string memberTelephoneNo, string trustReason)
+    {
+      if (string.IsNullOrEmpty(clientCorporateNo) || string.IsNullOrEmpty(memberTelephoneNo))
+      {
+        return null;
+      }
+
+      IMSACCO_TIMSI_RESET_LOG_PARAMS resetData = new MSaccoTIMSIBLL.TIMSIResetParams
+      {
+        CustomerPhoneNo = memberTelephoneNo.Replace("-", ""),
+        ResetNarration = trustReason,
+        ActionUser = User.Identity.Name,
+      };
+      bool isReset = _msaccoTIMSIBLL.ResetTIMSIMemberRecordForClient(clientCorporateNo, resetData, out string resetMessage);
+
+      return Json(new
+      {
+        success = isReset,
+        ex = resetMessage
+      }, JsonRequestBehavior.AllowGet);
+    }
+
+    #endregion
+  }
+}
---
+++ MSacco_Dataspecs/Models/ISACCO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSacco_Dataspecs.Models
+{
+  public interface ISACCO
+  {
+    string corporateNo { get; set; }
+    string corporateNo_2 { get; set; }
+    string databaseName { get; }
+    double mpesaFloat { get; set; }
+    string saccoName_1 { get; set; }
+    bool isActive { get; set; }
+    string tableName { get; }
+  }
+}
