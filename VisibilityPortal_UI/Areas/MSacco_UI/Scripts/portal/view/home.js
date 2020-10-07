c158e71558130b88a9b6eff234197243010bbf5b
madote <madote@coretec.co.ke>
2020-10-07 09:44:04 +0300

    07Oct2020_0943 MSACCO: Disable Financial Summary js alert in dashboard

--- VisibilityPortal_UI/Areas/MSacco_UI/Scripts/portal/view/home.js
+++ VisibilityPortal_UI/Areas/MSacco_UI/Scripts/portal/view/home.js
@@ -140,10 +140,10 @@ function DisplayFinancialSummaryForTheDay(datasetParams)
     error: function (xhr, ajaxOptions, thrownError) {
       //some errror, some show err msg to user and log the error
       //alert("Server Error Occurred. Unable to load summary");
-      bootbox.alert({
-        title: "<h3>Daily Financial Total</h3>",
-        message: "Server Error Occurred. Unable to load " + datasetParams.datasetName + " summary." 
-      })
+      //bootbox.alert({
+      //  title: "<h3>Daily Financial Total</h3>",
+      //  message: "Server Error Occurred. Unable to load " + datasetParams.datasetName + " summary." 
+      //})
       //Log(xhr.responseText);
     }
   });
