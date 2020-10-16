fa0fd370075ccd034fb72c4f40ec341720cc01e5
jandev-tinker <64497780+jandev-tinker@users.noreply.github.com>
2020-10-16 19:52:37 +0300

    16Oct2020_1950 MSACCO: Add view for IMSI reset logs
    Summary:
        * This follows ultimatum by Harambee SACCO (Amos Ndungu) that they will
        not use the IMSI Authentication functionality until they can "see logs
        of who reset what"

---
+++ VisibilityPortal_UI/Areas/MSacco_UI/Scripts/portal/view/msaccoTIMSIResetRecords.js
@@ -0,0 +1,76 @@
+﻿var tblTabulator;
+var tabulatorAjaxUrlForReload;
+var tabulatorAjaxParamsForReload;
+
+function initTabulator(tableContainerID) {
+  //create Tabulator on DOM element with id == tableContainerID
+  tblTabulator = new Tabulator(tableContainerID, {
+    height: 405, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+    //data: tabledata, //assign data to table
+    placeholder: "No Logged Authentication Record(s) Found ",
+    pagination: "remote", //enable remote pagination
+    paginationSize: 100,
+    paginationSizeSelector: true,
+    //ajaxProgressiveLoad: "scroll",
+    //ajaxURL: 'Loans/GetLoanRecords',
+    //ajaxResponse: function (url, params, response) {
+    //  //url - the URL of the request
+    //  //params - the parameters passed with the request
+    //  //response - the JSON object returned in the body of the response.
+
+    //  return response.data; //return the tableData property of a response json object
+    //},
+    // collapse columns that no longer fit on the table into a list under the row
+    responsiveLayout: "collapse",
+    responsiveLayoutCollapseStartOpen: false,
+    layout: "fitColumns", //"fitDataFill", //fit columns to width of table (optional),
+    headerFilterPlaceholder: "filter ...",
+    tooltipsHeader: true,
+    columns: [ //Define Table Columns
+      {
+        formatter: "responsiveCollapse", width: 30, minWidth: 30,
+        align: "center", resizable: false, headerSort: false
+      },
+      //{ title: "Subscriber ID", field: "LogNo" },
+      { title: "Member Phone Number", field: "CustomerPhoneNo", headerFilter: true },
+      { title: "Authenticated By", field: "ActionUser", headerFilter: true },
+      { title: "Supplied KYC Info", field: "ResetNarration", formatter: "textarea", headerFilter: true},
+      {
+        title: "TIMESTAMP", field: "ResetStatusDate",
+        align: "center",
+        formatter: function (cell, formatterParams) {
+          return GetFormattedDate(cell.getValue());
+        }
+      },
+
+    ],
+    movableColumns: true
+
+  });
+  $(tblTabulator.element).addClass("table table-striped table-condensed table-hover");
+}
+
+
+function GetFormattedDate(objDate) {
+  return (objDate == null) ?
+    'No Date' : moment(objDate).format("DD-MMM-YYYY hh:mm:ss A")
+}
+
+function ClearFilters() {
+  tblTabulator.clearFilter(true);
+}
+
+function LoadData(restUrl, corporateNo) {
+  if (corporateNo == '' || corporateNo == undefined) return;
+  var ajaxParams = { clientCorporateNo: corporateNo };
+
+  tabulatorAjaxUrlForReload = restUrl;
+  tabulatorAjaxParamsForReload = ajaxParams
+
+  ClearFilters();
+  tblTabulator.setData(restUrl, ajaxParams);
+}
+
+function ReloadData() {
+  tblTabulator.setData(tabulatorAjaxUrlForReload, tabulatorAjaxParamsForReload);
+}
