8f128f3d4a1f7ff03cbc9181ba94c109c063bc11
jandev-tinker <64497780+jandev-tinker@users.noreply.github.com>
2021-10-26 15:55:44 +0300

    26Oct2021_1552 Publish webApp_26Oct2021_1551
    Summary:
        * Adds batch whitelisting functionality on MSACCO

--- webApp/Areas/MSacco_UI/Scripts/portal/view/shared.js
+++ webApp/Areas/MSacco_UI/Scripts/portal/view/shared.js
@@ -12,3 +12,7 @@ function tabulatorClearFilters() {
 function TabulatorSetCellValueToNullIfNoData(cellValue, valueToSet = null) {
   return (cellValue == null) ? (valueToSet != null) ? valueToSet : "No Data" : cellValue
 }
+
+function DoesStringContainSubString(stringToCheck, subStringToLookFor) {
+  return stringToCheck.indexOf(subStringToLookFor) !== -1;
+}
