eddcb5713c749abd6b022a1868e3284a6620024e
jandev-tinker <64497780+jandev-tinker@users.noreply.github.com>
2022-07-19 01:17:18 +0300

    19Jul2022_0112 Publish webApp_19Jul2022_0039
    Summary:
        * This adds:
            - SMS Marketing functionality on CallCenter

---
+++ webApp/Areas/CallCenter_UI/Scripts/portal/view/msacco.js
@@ -0,0 +1,92 @@
+﻿(function () {
+    var msaccoClient = $.connection.msaccoClient;
+    msaccoClient.client.serverMessage = function (apiCommParams, response) {
+        if (response == '') {
+            response = { success: false, ex: "Server did not provide a response", title: "<h5>MSACCO</h5>" };
+        }
+        else {
+            try {
+                apiCommParams = JSON.parse(apiCommParams);
+                response = JSON.parse(MSACCODecryptor(apiCommParams.encSecret, apiCommParams.encKey, response));
+            }
+            catch (err) {
+                console.log(
+                    "Error processing disconnected callback. Failed interpreting response: " + err.message + " at " + moment().format(PORTAL_DATE_FORMAT));
+                response = {
+                    success: false,
+                    ex: "An error occurred interpreting the server's response. <br/><br/>",
+                    title: "<h5>MSACCO</h5>"
+                };
+            }
+        }
+        // Display message as toast
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": true,
+            "progressBar": true,
+            "positionClass": "toast-top-center",
+            "preventDuplicates": true,
+            "onclick": null,
+            "showDuration": "10000",
+            "hideDuration": "1000",
+            "timeOut": "0",
+            "extendedTimeOut": "0",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+        if (response.success) {
+            toastr["success"](response.ex, response.title);
+        }
+        else {
+            toastr["error"](response.ex, response.title);
+        }
+
+    }
+    
+    $.connection.hub.start()
+        .done(function () { console.log("Connected to MSACCO | " + moment().format(PORTAL_DATE_FORMAT) + " | Connection ID: " + $.connection.hub.id); })
+        .fail(function (error) { console.log("Failed connecting to MSACCO->"+error +" | " + moment().format(PORTAL_DATE_FORMAT)) });
+
+})();
+
+var requestType = {
+    GET: 'GET',
+    POST: 'POST'
+};
+
+var ERR_CODE = {
+    BAD_REQ: 400
+};
+
+var msaccoCallBack = {
+    SUCCESS: function (response) { },
+    ERROR: function (xhr, status, error) { }
+};
+
+
+
+function MSACCODecryptor(secretKey, token, dataToDecrypt) {
+    var key = CryptoJS.enc.Utf8.parse(secretKey);
+    var iv = CryptoJS.enc.Utf8.parse(token);
+
+    var decrypted = CryptoJS.AES.decrypt(dataToDecrypt, key, { iv: iv });
+
+    return decrypted.toString(CryptoJS.enc.Utf8);
+}
+
+function CallMSACCO(restUrl, reqData, apiCommParams, callbackSpec) {
+    $.ajax(restUrl, {
+        type: apiCommParams.requestType,
+        contentType: "application/x-www-form-urlencoded",
+        data: reqData,
+        dataType: "json",
+        headers: {
+            'XToken': apiCommParams.xToken
+        },
+        success: callbackSpec.SUCCESS,
+        error: callbackSpec.ERROR
+    });
+}
--- webApp/Areas/CallCenter_UI/Scripts/portal/view/shared.js
+++ webApp/Areas/CallCenter_UI/Scripts/portal/view/shared.js
@@ -1,6 +1,28 @@
-﻿function initSelectPicker(selectRef) {
+﻿var PORTAL_DATE_FORMAT = "DD-MMM-YYYY hh:mm:ss A";
+
+function initSelectPicker(selectRef) {
   $(selectRef).selectpicker({
     liveSearch: true,
     showTick: true
   });
+}
+
+function DoesStringContainSubString(stringToCheck, subStringToLookFor) {
+    return stringToCheck.indexOf(subStringToLookFor) !== -1;
+}
+
+function PopupMessage(alertTitle, alertMessage, titleOverride = null) {
+    var displayTitle;
+
+    if (titleOverride == null || titleOverride == undefined) {
+        displayTitle = "<h5>" + alertTitle + "</h5>"
+    }
+    else {
+        displayTitle = titleOverride;
+    }
+
+    bootbox.alert({
+        title: displayTitle,
+        message: alertMessage
+    });
 }
